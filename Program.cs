f66414de508b614228633f3f76fde2814d879a20
Michitaa <nicolldecoll2102@gmail.com>
2025-09-17 21:35:09 -0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C6_Semana2_2 
+{
+    class Program
+{
+    static void Main(string[] args)
+        {
+            var miRectangulo = new Rectangulo(10, 5);
+
+            double area = miRectangulo.CalcularArea();
+
+            Console.WriteLine($"El área del rectángulo es: {area}");
+
+            Console.ReadKey();
+        }
+    }
+}
